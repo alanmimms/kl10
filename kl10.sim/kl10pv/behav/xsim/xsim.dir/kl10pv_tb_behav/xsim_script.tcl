67a9b5ee4d9eb6a4ffeee4804f33117ae4da7784
Alan Mimms <alanmimms@gmail.com>
2020-02-15 12:01:03 -0800

    checkpoint

---
+++ kl10.sim/kl10pv/behav/xsim/xsim.dir/kl10pv_tb_behav/xsim_script.tcl
@@ -0,0 +1,1 @@
+xsim {kl10pv_tb_behav} -view {{/home/alan/kl10/kl10pv_tb_behav.wcfg}} -view {{/home/alan/kl10/kl10pv_tb_CLK-behav.wcfg}} -tclbatch {kl10pv_tb.tcl} -key {Behavioral:kl10pv:Functional:kl10pv_tb}
