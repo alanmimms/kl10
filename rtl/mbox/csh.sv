b80d15945f4323603da19f09c13324f17b085b34
Alan Mimms <alanmimms@gmail.com>
2020-02-28 11:31:08 -0800

    checkpoint, starting PMA

--- rtl/mbox/csh.sv
+++ rtl/mbox/csh.sv
@@ -10,18 +10,6 @@ module csh(iAPR APR,
            iPMA PMA
 );
 
-  // XXX temporary
-  initial begin
-    CSH.EBOX_RETRY_REQ = '0;
-    CSH.EBOX_T0_IN = '0;
-    CSH.MBOX_RESP_IN = '0;
-    CSH.PAGE_FAIL_HOLD = '0;
-    CSH.GATE_VMA_27_33 = '0;
-
-    CSH.PAR_BIT_A = '0;
-    CSH.PAR_BIT_B = '0;
-  end
-
   bit clk;
   bit MB_REQ, CHAN_REQ_EN, EBOX_CYC, CYC_TYPE_HOLD, READY_TO_GO, WRITEBACK_T2;
   bit EBOX_REQ_GRANT, EBOX_REQ_EN, NON_EBOX_REQ_GRANT;
