ca17a74d203acde0a5e1bece169279ff0944ab35
Alan Mimms <alanmimms@gmail.com>
2020-03-06 11:03:58 -0800

    Getting ready to hook EBOX to MBOX properly. Remove old naming convention and start using the right wiring

--- rtl/mbox.sv
+++ rtl/mbox.sv
@@ -1,14 +1,11 @@
 `timescale 1ns/1ns
 `include "ebox.svh"
 module mbox(input mboxClk,
-            input [13:35] EBOX_VMA,
-            input vmaACRef,
-            input [0:35] cacheDataWrite,
             input req,
             input read,
-            input PSE,
             input write,
 
+            input [0:35] cacheDataWrite,
             output bit [0:35] cacheDataRead,
 
             iAPR APR,
@@ -54,11 +51,11 @@ module mbox(input mboxClk,
   (.clk(mboxClk),
    .din(cacheDataWrite),
    .dout(cacheDataRead),
-   .addr(EBOX_VMA[24:35]),
+   .addr(VMA.VMA[24:35]),
    .wea(write));
 `else
   fake_mem mem0(.clka(mboxClk),
-                .addra(EBOX_VMA[24:35]), // XXX remove slice when using real memory
+                .addra(VMA.VMA[24:35]), // XXX remove slice when using real memory
                 .dina(cacheDataWrite),
                 .douta(cacheDataRead),
                 .ena(1'b1),
