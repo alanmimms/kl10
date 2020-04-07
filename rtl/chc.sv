6b9530452106184a5fe37f85bad73dcac41e6479
Alan Mimms <alanmimms@gmail.com>
2020-04-07 13:58:29 -0700

    Note Wire-OR of negative logic signals yields positive logic Wire-AND and fix various places this occurs

--- rtl/chc.sv
+++ rtl/chc.sv
@@ -3,4 +3,8 @@
 
 // M8533 CHC
 module chc(iCHC CHC);
+
+  // NOTE CHC2,CHC3,CHC4 include Wire-OR of signals by NAME and not
+  // directly wired together with lines on the schematic. Beware!
+
 endmodule
