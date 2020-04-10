b088182109350887a568280caa611bdf5fdd1262
Alan Mimms <alanmimms@gmail.com>
2020-04-09 19:41:29 -0700

    Hard code as an 8-output one-hot decoder

--- rtl/decoder.sv
+++ rtl/decoder.sv
@@ -1,10 +1,9 @@
 `timescale 1ns/1ns
 
 module decoder
-  #(parameter N = 8)
   (input en,
-   input [0:$clog2(N)-1] sel,
-   output bit [0:N-1] q);
+   input [0:2] sel,
+   output bit [0:7] q);
 
   always_comb begin
     q = '0;
