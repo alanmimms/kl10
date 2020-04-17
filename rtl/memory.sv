3776c5dfefcc8cc5e87794b69dbfb457ba40c004
Alan Mimms <alanmimms@gmail.com>
2020-04-16 18:09:39 -0700

    checkpoint

--- rtl/memory.sv
+++ rtl/memory.sv
@@ -8,13 +8,15 @@
 // * Implement BLKO PI diagnostic cycle support.
 // * Support interleaving.
 // * Implement hardware memory through DMA to DRAM shared with Linux.
-module memory(iSBUS.memory SBUS);
+// * Support ACKN of next word while VALID on current word
+module memory(input CROBAR,
+              iSBUS.memory SBUS);
 `define MEM_SIZE (256*1024)
 
 `ifdef KL10PV_TB
   bit [0:35] mem[`MEM_SIZE];
-  memPhase aPhase('0, mem, SBUS, SBUS.START_A, SBUS.ACKN_A, SBUS.DATA_VALID_A);
-  memPhase bPhase('1, mem, SBUS, SBUS.START_B, SBUS.ACKN_B, SBUS.DATA_VALID_B);
+  memPhase aPhase(CROBAR, '0, mem, SBUS, SBUS.START_A, SBUS.ACKN_A, SBUS.DATA_VALID_A);
+  memPhase bPhase(CROBAR, '1, mem, SBUS, SBUS.START_B, SBUS.ACKN_B, SBUS.DATA_VALID_B);
 `else
 `endif
 
@@ -32,7 +34,8 @@ endmodule
 // the SBUS on the NEXT edge of that phase. This was done to allow
 // synchronous operation with cabling (etc.) propagation delays always
 // less than the clock cycle time.
-module memPhase(input phase,
+module memPhase(input CROBAR,
+                input phase,
                 ref bit [0:35] memory[`MEM_SIZE],
                 iSBUS.memory SBUS,
                 input START,
@@ -42,78 +45,113 @@ module memPhase(input phase,
   bit [12:35] addr;             // Address base we start at for quadword
   bit [34:35] wo;               // Word offset of quadword
   bit [0:3] toAck;              // Words we have not yet ACKed
-  bit [0:3] toRead;             // Words we have not yet read and returned
 
-  // This set of states is used to track assert and deassert wait
-  // states for ACKN and VALID.
-  typedef enum {trsIdle, trsWait2More, trsWait1More} tResponseState;
+  enum {
+        mphIDLE,
+        mphACK,
+        mphREAD1,
+        mphREAD2,
+        mphWAIT1,
+        mphWAIT2,
+        mphWAIT3,
+        mphWAIT4
+        } state, next;
 
-  tResponseState acknState;
-  tResponseState validState;
-  
+  always_ff @(negedge SBUS.CLK_INT != phase) begin
+    if (CROBAR) $display($time, " CLOCK: CROBAR");
+
+    if (!CROBAR && (state != mphIDLE || next != mphIDLE))
+      $display($time, " CLOCK [IDLE]: state=%s  next=%s", state.name, next.name);
+
+    if (CROBAR) state <= mphIDLE; // Initialize state machine
+    else state <= next;           // Advance state machine
+  end
+
+  // Generate state machine next state
+  always_comb begin
+
+    case (state)
+    mphIDLE:
+      if (START) begin
+        next = mphACK;
+        $display($time, " comb START==> IDLE->ACK");
+      end else begin
+        next = mphIDLE;
+        $display($time, " comb no START==> IDLE->IDLE");
+      end
+
+    mphACK:
+      if (toAck[0]) begin
+        next = mphREAD1;
+        $display($time, " comb ACK->READ1 toAck[0] != 0");
+      end else if (toAck == '0) begin
+        next = mphWAIT1;
+        $display($time, " comb ACK->WAIT1 toAck == 0");
+      end
+
+    mphREAD1: begin
+      next = mphREAD2;
+      $display($time, " comb READ1->READ2");
+    end
+
+    mphREAD2: begin
+      next = mphACK;
+      $display($time, " comb READ2->ACK");
+    end
+
+    mphWAIT1: next = mphWAIT2;
+    mphWAIT2: next = mphWAIT3;
+    mphWAIT3: next = mphWAIT4;
+    mphWAIT4: next = mphIDLE;
+    endcase
+  end
+
+  // Generate state machine outputs
   always_ff @(negedge SBUS.CLK_INT != phase) begin
 
-    // If we have completed all reads and acks and we see START it's a
-    // new cycle.
-    if (START && toAck == '0 && toRead == '0) begin
-      toAck <= SBUS.RQ;         // Still to ACK
-      toRead <= SBUS.RQ;        // Still to read and return
-      addr <= SBUS.ADR;         // Address of first word we do
-      wo <= SBUS.ADR[34:35];    // Word offset we increment mod 4
+    case (state)
+    mphIDLE:
+      if (START) begin
+        $display($time, " CLOCK OUT [IDLE]: SBUS.RQ=%4b, SBUS.ADR=%8o", SBUS.RQ, SBUS.ADR);
+        toAck <= SBUS.RQ;         // Still to ACK
+        addr <= SBUS.ADR;         // Address of first word we do
+        wo <= SBUS.ADR[34:35];    // Word offset we increment mod 4
+        ACKN <= '0;
+        VALID <= '0;
+      end
 
-      ACKN <= '0;
+    mphACK: begin
+      $display($time, " CLOCK OUT [ACK]:                                        clear VALID");
       VALID <= '0;
-      $display($time, " START: SBUS.RQ=%4b", SBUS.RQ);
-    end else begin
-      // ^^^ else is up there because we want to do first ack or valid
-      // data on the NEXT cycle after START.
-      
-      // If this word needs to be ACKed or is being ACKed...
+
       if (toAck[0]) begin
-
-        if (ACKN) begin         // If the ACK for this word is
-                                // CURRENTLY asserted, we deassert it
-                                // and move on to the next word.
-          $display($time, " toAck DEASSERT-SHIFT: toAck=%4b", toAck);
-          toAck <= {toAck[1:3], '0};
-        end else begin          // Else we will assert it and stay on
-                                // this word.
-          $display($time, " toAck   ASSERT-SHIFT: toAck=%4b", toAck);
-        end
-
-        ACKN <= ~ACKN;          // Switch ACK state
-      end else if (toAck != '0) begin
-        assert(!ACKN);
-        $display($time, " toAck SHIFT-ONLY: toAck=%4b", toAck);
-        toAck <= {toAck[1:3], '0}; // Move on to next possible ACK
+        $display($time, " CLOCK OUT [ACK]:              set ACKN");
+        ACKN <= '1;
       end
 
-      // If this word needs to be returned or VALID is asserted...
-      if (toRead[0]) begin
-
-        if (VALID) begin        // If VALID for this word is CURRENTLY
-                                // asserted, we deassert it and move
-                                // on to the next word.
-          $display($time, " toRead DEASSERT-SHIFT: toRead=%4b", toRead);
-
-          // Move forward to next word (modulo 4) to return.
-          toRead <= {toRead[1:3], '0};
-          wo <= wo + '1;
-        end else begin          // Else we will drive data and assert
-                                // VALID and stay on this word.
-          $display($time, " toRead   ASSERT-SHIFT: toRead=%4b", toRead);
-          SBUS.D <= memory[{addr[12:33], wo}];
-        end
-
-        VALID <= ~VALID;      // Switch VALID state
-      end else if (toRead != '0) begin
-        assert(!VALID);
-        $display($time, " toRead SHIFT-ONLY: toRead=%4b", toRead);
-
-        // Move forward to next word (modulo 4) to return.
-        toRead <= {toRead[1:3], '0};
-        wo <= wo + '1;
-      end
+      toAck <<= '1;
+      $display($time, " CLOCK OUT [ACK]: shift up from toAck=%4b", toAck);
     end
+
+    mphREAD1: begin
+      $display($time, " CLOCK OUT [READ1]             clear ACKN");
+      ACKN <= '0;
+    end
+
+    mphREAD2: begin
+      VALID <= '1;
+      SBUS.D <= memory[{addr[12:33], wo}];
+      SBUS.DATA_PAR <= ~(^memory[{addr[12:33], wo}]);
+      $display($time, " CLOCK OUT [READ2]: mem[%8o]=%s  ++wo   set VALID",
+               {addr[12:33], wo}, octW(memory[{addr[12:33], wo}]));
+      wo <= wo + 2'b01;
+    end
+
+    default: ;
+    endcase
   end
+
+  function string octW(input [0:35] w);
+    $sformat(octW, "%06o,,%06o", w[0:17], w[18:35]);
+  endfunction
 endmodule
