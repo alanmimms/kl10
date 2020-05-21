e1ddaf49424ada1f362a72e062b35b0c098e2dda
Alan Mimms <alanmimms@gmail.com>
2020-05-21 16:52:29 -0700

    Fix this so bits are not in backward order and use casex which actually works

--- rtl/priority-encoder8.sv
+++ rtl/priority-encoder8.sv
@@ -2,19 +2,19 @@
 
 module priority_encoder8
   (input [0:7] d,
-   output bit any,
-   output bit [0:2] q);
+   output bit [0:2] q,
+   output bit any);
 
   bit found;
-  always_comb priority case (d)
-              default: {any, q} = 4'b0000;
-              8'b???????1: {any, q} = 4'b1000;
-              8'b??????10: {any, q} = 4'b1001;
-              8'b?????100: {any, q} = 4'b1010;
-              8'b????1000: {any, q} = 4'b1011;
-              8'b???10000: {any, q} = 4'b1100;
-              8'b??100000: {any, q} = 4'b1101;
-              8'b?1000000: {any, q} = 4'b1110;
-              8'b10000000: {any, q} = 4'b1111;
+  always_comb casex (d)
+              8'b1xxxxxxx: {any, q} = 4'b1000;
+              8'b01xxxxxx: {any, q} = 4'b1001;
+              8'b001xxxxx: {any, q} = 4'b1010;
+              8'b0001xxxx: {any, q} = 4'b1011;
+              8'b00001xxx: {any, q} = 4'b1100;
+              8'b000001xx: {any, q} = 4'b1101;
+              8'b0000001x: {any, q} = 4'b1110;
+              8'b00000001: {any, q} = 4'b1111;
+              default:     {any, q} = 4'b0000;
               endcase
 endmodule
